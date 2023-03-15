586728bc5058e4b58f180a27a93c80af86768947
andreymmonteiro <andreymuti@hotmail.com>
2023-03-15 15:28:09 -0300

    Changed logic

--- Katas/Code/Features/RegexValidadePIN/Application/ValidatePIN.cs
+++ Katas/Code/Features/RegexValidadePIN/Application/ValidatePIN.cs
@@ -5,10 +5,6 @@ namespace Code.Features.RegexValidadePIN.Application
     internal class ValidatePIN : IValidatePIN
     {
         public bool IsValidPIN(string pin)
-        {
-            if (pin.Length == 4 || pin.Length == 6)
-                return !pin.Any(a => !int.TryParse(a.ToString(), out var value));
-            return false;
-        }
+            => (pin.Length == 4 || pin.Length == 6) && pin.All(Char.IsDigit);
     }
 }
